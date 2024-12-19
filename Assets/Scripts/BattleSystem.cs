73f76fc87a5f9aeed7533f875a8faa02ef436e14
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-20 00:24:28 +0100

    Het blokkeer functionaliteit voor level 3 toegevoegd aan de enemy

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
@@ -52,56 +53,74 @@ public class BattleSystem : MonoBehaviour
 
 	IEnumerator PlayerAttack()
 	{
-		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+		int action = Random.Range(0, 2);
+		Scene activeScene = SceneManager.GetActiveScene();
 
+		if (activeScene.name == "Level3")
+		{
+			if (action == 1)
+			{
+				dialogueText.text = "Enemy blokkeert de attack!!";
+				yield return new WaitForSeconds(2f);
+				state = BattleState.ENEMYTURN;
+				StartCoroutine(EnemyTurn());
+				yield break;
+			}
+		}
+		
+		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
 		enemyHUD.SetHp(enemyUnit.currentHealth);
 		dialogueText.text = "De aanval is succesvol!";
-
+		
 		yield return new WaitForSeconds(2f);
 
 		if(isDead)
 		{
 			state = BattleState.WON;
 			EndBattle();
-		} else
-		{
-			state = BattleState.ENEMYTURN;
-			StartCoroutine(EnemyTurn());
-		}
+			yield break;
+		} 
+		
+		state = BattleState.ENEMYTURN;
+		StartCoroutine(EnemyTurn());
 	}
 
 	IEnumerator EnemyTurn()
 	{
 		
-		int randomDamage = enemyUnit.GenerateRandomDamage(1, 11);
-		bool isDead = playerUnit.TakeDamage(randomDamage);
 		int action = Random.Range(0, 2);
-		Debug.Log(enemyUnit.currentHealth);
-
-		if (action == 0)
+		Scene activeScene = SceneManager.GetActiveScene();
+		
+		
+		if (activeScene.name == "Level2" || activeScene.name == "Level3")
 		{
-			dialogueText.text = enemyUnit.unitName + " doet " + randomDamage + " damage!";
-			playerHUD.SetHp(playerUnit.currentHealth);
-		}
-		else
-		{
-			dialogueText.text = enemyUnit.unitName + " healt zichzelf!";
-			enemyUnit.Heal(5);
-			enemyHUD.SetHp(enemyUnit.currentHealth);
-			Debug.Log(enemyUnit.currentHealth);
+			if (action == 1)
+			{
+				dialogueText.text = enemyUnit.unitName + " healt zichzelf!";
+				enemyUnit.Heal(5);
+				enemyHUD.SetHp(enemyUnit.currentHealth);
+				yield return new WaitForSeconds(1f);
+				state = BattleState.PLAYERTURN;
+				PlayerTurn();
+				yield break;
+			}
 		}
 		
+		int randomDamage = enemyUnit.GenerateRandomDamage(1, 11);
+		bool isDead = playerUnit.TakeDamage(randomDamage);
+		dialogueText.text = enemyUnit.unitName + " doet " + randomDamage + " damage!";
+		playerHUD.SetHp(playerUnit.currentHealth);
 		yield return new WaitForSeconds(1f);
-
+		
 		if(isDead)
 		{
 			state = BattleState.LOST;
 			EndBattle();
-		} else
-		{
-			state = BattleState.PLAYERTURN;
-			PlayerTurn();
-		}
+			yield break;
+		} 
+		
+		state = BattleState.PLAYERTURN;
+		PlayerTurn();
 
 	}
 
